dba1e42e32aae907ee2240235babc6690c834eea
shovan777 <raj.shrestha778@gmail.com>
2018-09-17 15:41:10 +0545

    Why am i commitiing

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+socket.on('connect', function () {
+  console.log('connected to the server');
+
+  socket.emit('createEmail', {
+    to: 'shovan777@gmail.com',
+    text: 'hey. whats up'
+  });
+
+  socket.emit('createMessage', {
+    from: 'beta',
+    text: 'hello papa. I do fine.'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('disconnected from the server');
+});
+
+socket.on('newEmail', function (email) {
+  console.log('New email has arrived', email);
+});
