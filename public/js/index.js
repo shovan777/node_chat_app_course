47dc936d7b059150711bb57cc903fc88d4ca91d7
shovan777 <raj.shrestha778@gmail.com>
2018-09-20 16:26:29 +0545

    Add moment for formatted time

--- public/js/index.js
+++ public/js/index.js
@@ -15,8 +15,9 @@ socket.on('connect', function () {
 
 socket.on('newMessage', function(msg) {
   console.log('new msg has arrived', msg);
+  var formattedTime = moment(msg.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${msg.from}: ${msg.text}`);
+  li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
   jQuery('#messages').append(li);
 });
 
@@ -38,11 +39,13 @@ socket.on('disconnect', function () {
 //
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function (ackMsg) {
     console.log('got it', ackMsg);
+    messageTextBox.val('');
   });
 });
 
@@ -52,25 +55,32 @@ locationButton.on('click', function () {
   if(!navigator.geolocation) {
     return alert('geolocation not supported by browser');
   }
+// disable the button until location task has been completed
+  locationButton.attr('disabled', 'disabled').text('Sending location....');
 
   navigator.geolocation.getCurrentPosition(
+    // locationButton.removeAttr('disabled')
     function (position) {
+      locationButton.removeAttr('disabled').text('Send Location');
       console.log(position);
+
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     }, function () {
+      locationButton.removeAttr('disabled').text('Send Location');
       alert('Unable to fetch user location');
     });
 });
 
 socket.on('newLocationMessage', function (message) {
+  var formattedTime = new moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   // _blank tells to open the link in new tab
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
